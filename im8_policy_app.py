9cb033a2195b5353db4791e84182b7e84ddda3c5
prarthana <prarthana.shah@data-axle.com>
2023-09-07 06:27:53 +0530

    added doccuments and updated app

--- im8_policy_app.py
+++ im8_policy_app.py
@@ -10,7 +10,6 @@ sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 import os
 
-from langchain.llms import OpenAI
 from langchain.vectorstores import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import ConversationalRetrievalChain
@@ -20,22 +19,12 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain import PromptTemplate
 from pdfminer.high_level import extract_pages
 
-# from pdfminer.layout import LTTextContainer
-# Create a file uploader
-
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 st.set_page_config(page_title='LLM Summarization Docs')
 st.title('🦜🔗 LLM Summarization')
 
-if 'uploaded_file' not in st.session_state:
-    st.session_state.uploaded_file = None
-
-if 'current_filename' not in st.session_state:
-    st.session_state.current_filename = None
-
-if "QA" not in st.session_state:
-    st.session_state.QA = []
-
+# initialize default variables
+st.session_state.QA = []
 LLMDATA = {}
 
 
@@ -77,36 +66,35 @@ def set_LLM(uploaded_file):
             print(len(LLMDATA))
 
 
-def generate_response(query_text, filename):
-    try:
-        # Trying to access the 'LLMDATA' attribute in the 'session_state' object
-        LLMDATA = st.session_state.LLMDATA
-    except AttributeError:
-        # Handling the AttributeError
-        st.write("Please submit the uploaded file.")
-        # You can choose to perform alternative actions here if needed
-    except Exception as e:
-        # Handling any other exceptions
-        st.write(f"An unexpected error occurred: {e}")
-        raise e
-    if filename in LLMDATA:
-        db = LLMDATA[filename]["db"]
-        system_template = """
-        You are an intelligent bot, excellent at finding answers from the documents.
-        I will ask questions from the documents and you'll help me try finding the answers from it.
-        Give the answer using best of your knowledge, say you dont know if not able to answer.
-        ---------------
-        {context}
-        """
-        qa_prompt = PromptTemplate.from_template(system_template)
-        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-        qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0, model_name="gpt-4"), db.as_retriever(),
-                                                   memory=memory, condense_question_prompt=qa_prompt)
-        result = qa({"question": query_text})
-        # return result["answer"]   
-        dict = {"question": result["question"], "answer": result["answer"]}
-        st.session_state.QA.append(dict)
-
+def generate_response(query_text):
+    # try:
+    #     # Trying to access the 'LLMDATA' attribute in the 'session_state' object
+    #     LLMDATA = st.session_state.LLMDATA
+    # except AttributeError:
+    #     # Handling the AttributeError
+    #     st.write("Please submit the uploaded file.")
+    #     # You can choose to perform alternative actions here if needed
+    # except Exception as e:
+    #     # Handling any other exceptions
+    #     st.write(f"An unexpected error occurred: {e}")
+    #     raise e
+    # if filename in LLMDATA:
+    #     db = LLMDATA[filename]["db"]
+    system_template = """
+    You are an intelligent and excellent at finding answers from the documents.
+    I will ask questions from the documents and you'll help me try finding the answers from it.
+    Give the answer using best of your knowledge, say you dont know if not able to answer.
+    ---------------
+    {context}
+    """
+    qa_prompt = PromptTemplate.from_template(system_template)
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0, model_name="gpt-4"), db.as_retriever(),
+                                               memory=memory, condense_question_prompt=qa_prompt)
+    result = qa({"question": query_text})
+    # return result["answer"]
+    dict = {"question": result["question"], "answer": result["answer"]}
+    st.session_state.QA.append(dict)
 
 def file_upload_form():
     with st.form('fileform'):
@@ -130,9 +118,8 @@ def query_form():
         query_text = st.text_input('Enter your question:', placeholder='Enter your question here')
         submitted = st.form_submit_button('Submit', disabled=(query_text == ""))
         if submitted:
-            filename = st.session_state.current_filename
             with st.spinner('Generating...'):
-                generate_response(query_text, filename)
+                generate_response(query_text)
                 for i in st.session_state.QA:
                     st.write("Question : " + i["question"])
                     st.write("Answer : " + i["answer"])
